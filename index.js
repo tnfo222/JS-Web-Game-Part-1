973ed3df6e648285481f40732866c3deb560be7c
tnfo222 <github.com/tnfo222/psychic-disco>
2022-05-05 21:44:36 -0400

    Added images and items to the game

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+//function to insert new items at different locations
+function newImage(asset,left,bottom){
+    let item = document.createElement('img')
+    item.src = asset
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom + 'px'
+    document.body.append(item)
+    return item
+}
+//Images to be inserted
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+//function to insert and collect items
+function newItem(asset,left,bottom){
+    let item = newImage(asset,left,bottom)
+    document.body.append(item)
+
+    item.addEventListener('dblclick', () => {
+        item.remove()
+    })
+}
+
+//Items to insert and collect
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
